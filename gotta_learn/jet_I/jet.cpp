f2a7b3d26444c52ef07cd1f4d8ea398eeae690f8
LeiSurrre <ben.yutong.chen@hotmail.com>
2020-04-02 19:26:47 -0400

    jet I

---
+++ gotta_learn/jet_I/jet.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main() {
+  int i,j;
+  int x = 10;
+  int y = 5;
+  char input;
+
+  while(1) {
+    //system("cls");
+    for(j=0;j<y;j++) {
+      printf("\n");
+    }
+  }
+
+  return 0;
+}
