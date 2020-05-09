3c1c1b3a2e22131871b41e8196e3981afab15502
LeiSurrre <ben.yutong.chen@hotmail.com>
2020-05-08 22:17:19 -0400

    fix bug in cell

--- gotta_learn/cell/cell.cpp
+++ gotta_learn/cell/cell.cpp
@@ -20,9 +20,9 @@ void gotoxy(int x, int y) {
 void startup() {
 	int i, j;
 	srand(time(NULL));
-	for (i = 0; i < W; i++) {
-		for (j = 0; j < H; j++) {
-			cells[i][j] = rand() % 2;
+	for (j = 0; j < H; j++) {
+		for (i = 0; i < W; i++) {
+			cells[j][i] = rand() % 2;
 		}
 	}
 }
@@ -47,8 +47,8 @@ void updateWithoutInput() {
 	int newCells[H][W];
 	int n;
 	int i, j;
-	for (j = 1; j <= H - 1; j++) {
-		for (i = 1; i <= W - 1; i++) {
+	for (j = 1; j < H - 1; j++) {
+		for (i = 1; i < W - 1; i++) {
 			n = cells[j - 1][i - 1] + 
 				cells[j - 1][i] +
 				cells[j - 1][i + 1] +
@@ -58,18 +58,18 @@ void updateWithoutInput() {
 				cells[j + 1][i] +
 				cells[j + 1][i + 1];
 			if (n == 3) {
-				newCells[i][j] = 1;
+				newCells[j][i] = 1;
 			} else if (n == 2) {
-				newCells[i][j] = cells[i][j];
+				newCells[j][i] = cells[j][i];
 			} else {
-				newCells[i][j] = 0;
+				newCells[j][i] = 0;
 			}
 		}
 	}
 
 	for (j = 1; j <= H - 1; j++) {
 		for (i = 1; i <= W - 1; i++) {
-			cells[i][j] = newCells[i][j];
+			cells[j][i] = newCells[j][i];
 		}
 	}
 }
