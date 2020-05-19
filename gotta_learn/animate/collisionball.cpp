216d69e79a7edcbe52eeacf4850a4efec3a89749
LeiSurrre <ben.yutong.chen@hotmail.com>
2020-05-18 23:06:40 -0400

    finished collisionball

--- gotta_learn/animate/collisionball.cpp
+++ gotta_learn/animate/collisionball.cpp
@@ -1,21 +1,22 @@
 #include <graphics.h> // EasyX library https://docs.easyx.cn/en-us/intro
 #include <time.h>
+#include <math.h>
 #include <conio.h>
 
 #define HIGH 480
 #define WIDTH 640
-#define BALLN 5
+#define BALLN 10
 
 int main() {
     int balls[BALLN][2];
     int ballv[BALLN][2];
     int radius = 20;
-    int i;
+    int i, j;
 
     srand(time(NULL));
     for (i = 0; i < BALLN; i++) {
-        balls[i][0] = (i + 2) * radius * 3;
-        balls[i][1] = HIGH / 2;
+        balls[i][0] = rand() % int(WIDTH - 4 * radius) + 2 * radius;
+        balls[i][1] = rand() % int(HIGH - 4 * radius) + 2 * radius;
         ballv[i][0] = (rand() % 2) * 2 - 1;
         ballv[i][1] = (rand() % 2) * 2 - 1;
     }
@@ -36,6 +37,12 @@ int main() {
         for (i = 0; i < BALLN; i++) {
             balls[i][0] += ballv[i][0];
             balls[i][1] += ballv[i][1];
+
+            // fix ball out of boundary
+            if (balls[i][0] < radius) balls[i][0] = radius;
+            if (balls[i][1] < radius) balls[i][1] = radius;
+            if (balls[i][0] > WIDTH - radius) balls[i][0] = WIDTH - radius;
+            if (balls[i][1] > HIGH - radius) balls[i][1] = HIGH - radius;
         }
 
         // hit wall
@@ -45,6 +52,43 @@ int main() {
             }
             if (balls[i][1] <= radius || balls[i][1] >= HIGH - radius) {
                 ballv[i][1] = -ballv[i][1];
+            }
+        }
+
+        float minDistanceSquare[BALLN][2];
+        for (i = 0; i < BALLN; i++) {
+            minDistanceSquare[i][0] = 99999999; // distance square
+            minDistanceSquare[i][1] = -1; // index
+        }
+
+        // calculate squared distance
+        for (i = 0; i < BALLN; i++) {
+            for (j = 0; j < BALLN; j++) {
+                if (i != j) {
+                    float distSquare = (balls[i][0] - balls[j][0]) * (balls[i][0] - balls[j][0])
+                        + (balls[i][1] - balls[j][1]) * (balls[i][1] - balls[j][1]);
+                    if (distSquare < minDistanceSquare[i][0]) {
+                        minDistanceSquare[i][0] = distSquare;
+                        minDistanceSquare[i][1] = j;
+                    }
+                }
+            }
+        }
+
+        //collision check
+        for (i = 0; i < BALLN; i++) {
+            if (minDistanceSquare[i][0] <= 4 * radius * radius) { // (2*radius)^2
+                j = minDistanceSquare[i][1];
+                int temp;
+                temp = ballv[i][0];
+                ballv[i][0] = ballv[j][0];
+                ballv[j][0] = temp;
+                temp = ballv[i][1];
+                ballv[i][1] = ballv[j][1];
+                ballv[j][1] = temp;
+
+                // avoid duplicated check
+                minDistanceSquare[j][0] = 99999999;
             }
         }
 
